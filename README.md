3f959a02234eab78248cec0d761996b67971e53a
Chansu Park <hhosu107@gmail.com>
2017-11-02 18:29:55 +0900

    Start using FUNCTION, but have to learn how to use it as a template

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 iRRAM implementation of ADT and algorithms for Exact calculuation of Vector Calculus
 
 ## iRRAM
-Repository: [Github](https://github.com/hhosu107/iRRAM) (Forked and edited some argument of [iRRAM](https://github.com/fbrausse/iRRAM/tree/2df92ddb4476c450d67a41b2b4c8ed65137d6b8d))
-Commit: 632889c3db554803e0e34756651cd769660583c2
+Repository: [Github](https://github.com/fbrausse/iRRAM)
+Commit: bf9cd31f1e10854be3720a8a436d64ea88232cdd
--- src/lipschitz-conti.h
+++ src/lipschitz-conti.h
@@ -2,9 +2,12 @@
 #define LIPSCHITZ_CONTI_H
 
 #include "iRRAM/core.h"
+#include "iRRAM/FUNCTION.h"
 
 #include<vector>
 #include<stack>
+#include<functional>
+#include<memory>
 
 using namespace iRRAM;
 
@@ -21,11 +24,12 @@ class Rational {
     }
 };
 
+template<class RESULT,class... PARAM>
 class Lipschitz {
   public:
     virtual ~Lipschitz() = 0;
-    virtual fp = 0;
-    virtual unsigned int dimension() = 0; // Domain is a subset of R^d
+    virtual FUNCTION<RESULT, PARAM> fp = 0; // inspired from FUNCTION.h
+    virtual unsigned int dimension = 0; // Domain is a subset of R^d
     virtual std::vector<unsigned int> deriv = 0; // k = (k_1, \dots, k_d) continuously differentiable
     virtual unsigned int lip = 0; // fp^(k) is lip-lipschitz conti
     virtual REAL apply(std::vector<REAL> x) = 0;
